e5be1491077eebf88704313a03967234e7ab5009
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-31 22:20:17 +0530

    new code

---
+++ poem.c
@@ -0,0 +1,20 @@
+// write a program to print any poem.
+#include<stdio.h>
+int main()
+{
+    printf("\t\t\t---:DREAM:---\n");
+    printf("\t\tHOLD FAST TO DREAMS.\n");
+    printf("\t\tFOR IF DREAMS DIE. \n");
+    printf("\t\tLIFE IS A BROKEN-WINGED BIRDS. \n");
+    printf("\t\tTHAT CONNECT FLY. \n");
+    printf("\n");
+    printf("\n");
+    printf("\t\tHOLD FAST TO DREAMS. \n");
+    printf("\t\tFOR WHEN DREAMS GO. \n");
+    printf("\t\tLIFE IS A BORREN FIELD. \n");
+    printf("\t\tFROZEN WITH SNOW. \n");
+    printf("\n");
+    printf("\t\t\t\t---LANGSTOR HUGHES. \n");
+    
+    return 0;
+}
