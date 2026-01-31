8ff63cd2e0f06b7430d8c04edc437f06ccf79360
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-31 22:08:45 +0530

    new code

---
+++ introduction.c
@@ -0,0 +1,20 @@
+// write a program to print seelf introduction.
+#include<stdio.h>
+int main()
+{
+    printf("\t\t\t---:SELF INTRODUCTION:---\n");
+    printf("\t\tMy name is Sweta. \n");
+    printf("\t\tI am 21 years old. \n");
+    printf("\t\tI am from Patna. \n");
+    printf("\t\tLet me tell you about schooling. \n");
+    printf("\t\tI studied at Mother Teresa Tiny Tot's. \n");
+    printf("\t\tI complete my junior college education from PLS college. \n");
+    printf("\t\tAnd Now i am BCA student. \n");
+    printf("\t\tMy parents are the piller of my strenght. \n");
+    printf("\t\tMy favourite subject is vb.net & sql. \n");
+    printf("\t\tMy hobbies are drawing,panting, and listening music. \n");
+    printf("\t\tMy goal is to become a software analyst. \n");
+    printf("\n\t\t\t\t\tTHANK YOU VERY MUCH. \n");
+    
+    return 0;
+}
