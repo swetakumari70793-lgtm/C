201bdee3f25ae42ea3d74cf809c64130f5cc7932
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-31 22:49:19 +0530

    new code

---
+++ keyward.c
@@ -0,0 +1,21 @@
+//write a program to perform use to keyward and run program.
+#include<stdio.h>
+int main()
+{
+    int a = 5500;
+    float f = 77.89;
+    char ch = 's';
+    double d = 566.898888888888;
+    long int li = 76000000;
+    short int si = 89;
+    char std_name [20] = "gautam";
+
+    printf("\n Integer value is  %d\n",a);
+    printf("\n Floating value is  %f\n",f);
+    printf("\n Character value is  %c\n",ch);
+    printf("\n Double value is  %f\n",d);
+    printf("\n Long Integer value is  %ld\n",li);
+    printf("\n Short Integer value is  %d\n",si);
+    printf("\n string value is  %s\n",std_name);
+    return 0;
+}
