3bf3f61027843c4f2f2d46f4ef3e9e157a4398df
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-01 22:45:49 +0530

    new code

--- add.c
+++ add.c
@@ -1,9 +1,11 @@
-//sum two number.
+// Write a program to enter any two number from user and add them.
 #include<stdio.h>
 int main()
 {
-    int a=5 , b=10 , c;
-    c=a+b;
-    printf("sum is a %d+%d=%d\n",a,b,c);
+    int a , b , add;
+    printf("Enter any two number ");
+    scanf("%d %d",&a,&b);
+    add=a+b;
+    printf("Addition is %d + %d = %d ",a,b,add);
     return 0;
 }
