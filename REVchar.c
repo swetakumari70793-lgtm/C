3e7b1ce271c9c8478bba8731f21dd1cf2808dec2
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-05 23:05:08 +0530

    new code

---
+++ REVchar.c
@@ -0,0 +1,17 @@
+//Write a C program to print the following characters in reverse.
+
+#include<stdio.h>
+int main()
+{
+    char ch1 = 'S';
+    char ch2 = 'W';
+    char ch3 = 'E';
+    char ch4 = 'T';
+    char ch5 = 'A';
+
+    printf("\n");
+    printf("The reverse of %c%c%c%c%c is %c%c%c%c%c\n",ch1,ch2,ch3,ch4,ch5,ch5,ch4,ch3,ch2,ch1 );
+    printf("\n");
+    return 0;
+    
+}
