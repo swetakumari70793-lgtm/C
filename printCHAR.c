d7c0baf773fb2d220ab76f22e1df068e0c950706
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-05 23:25:41 +0530

    new code

---
+++ printCHAR.c
@@ -0,0 +1,39 @@
+/*Write a C program to print a block F using the hash (#), 
+where the F has a height of six characters and width of five and four characters. 
+And also print a very large 'C'.
+
+######
+#
+#####
+#
+#
+#
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("\n");
+    printf("######\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("\n");
+
+    printf("\n");
+    printf("\n");
+    printf("  ##########\n");
+    printf("##\t   ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("##\t   ##\n");
+    printf("  ##########\n");
+
+    return 0;
+}
